688634086a94ac6717bef5a97371fd96564f85cc
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-23 08:15:13 +0700

    fix(button): button component layout when using icon

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,22 +2,24 @@ import { cva } from "class-variance-authority";
 import clsx from "clsx";
 
 const buttonVariants = cva(
-  "rounded-lg font-semibold transition-all transform hover:scale-105 focus:outline-none cursor-pointer",
+  "rounded-lg font-semibold transition-all transform hover:scale-105 focus:outline-none cursor-pointer inline-flex items-center justify-center",
   {
     variants: {
       variant: {
         primary: "bg-surface text-foreground shadow-lg hover:bg-surface/90",
         secondary: "bg-muted text-text-secondary hover:bg-muted/80",
         outline:
-          "border-2 border-border text-subtle-text hover:bg-surface hover:text-foreground",
+          "border border-border text-subtle-text hover:bg-surface hover:text-foreground",
         ghost: "text-subtle-text hover:bg-surface hover:text-foreground",
+        link: "text-foreground underline-offset-4 hover:underline ",
+        icon: "text-foreground",
         destructive: "bg-red-600 text-white shadow-lg hover:bg-red-700",
         muted: "bg-muted text-text-secondary cursor-not-allowed opacity-70",
       },
       size: {
-        sm: "px-4 py-2 text-sm",
-        md: "px-6 py-3 text-base",
-        lg: "px-8 py-4 text-lg",
+        sm: "px-4 py-2 text-sm leading-none",
+        md: "px-6 py-3 text-base leading-none",
+        lg: "px-8 py-4 text-lg leading-none",
       },
       width: {
         auto: "w-auto",
@@ -27,7 +29,7 @@ const buttonVariants = cva(
     },
     defaultVariants: {
       variant: "primary",
-      size: "lg",
+      size: "md",
       width: "auto",
     },
   }
