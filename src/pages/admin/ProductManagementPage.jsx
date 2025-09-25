26bf40ac4f5466126ef3a1652899b722cb30d330
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-25 20:50:16 +0700

    fix(api): use correct endpoint for admin getProducts

--- src/pages/admin/ProductManagementPage.jsx
+++ src/pages/admin/ProductManagementPage.jsx
@@ -11,13 +11,16 @@ const ProductManagementPage = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/products/me", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await fetch(
+        "http://localhost:3000/api/products/admin/me",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       const data = await response.json();
 
       if (!response.ok) {
