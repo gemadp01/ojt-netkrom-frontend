3b2bf08275de603ea89773ce24de0c71e504af15
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-26 09:08:06 +0700

    WIP: Admin Profile Page

---
+++ src/pages/admin/AdminProfilePage.jsx
@@ -0,0 +1,7 @@
+import AdminLayout from "@/components/AdminLayout/AdminLayout";
+
+const AdminProfilePage = () => {
+  return <AdminLayout title="Profile">Coming soon...</AdminLayout>;
+};
+
+export default AdminProfilePage;
